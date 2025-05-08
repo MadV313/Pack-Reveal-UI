73447d5c0763e2436a657517259eb2d716c44f75
MadV313 <snowvonia@gmail.com>
2025-05-08 12:30:16 -0400

    Update packReveal.js

--- scripts/packReveal.js
+++ scripts/packReveal.js
@@ -28,6 +28,7 @@ async function packReveal() {
       filename: c.filename,
       rarity: c.rarity,
       isNew: c.isNew,
+      owned: c.owned, // Include owned count in transfer
       number: c.card_id?.replace('#', '') || '',
       name: c.name || ''
     }))
@@ -140,59 +141,62 @@ async function packReveal() {
   }
 
   function generateMockPack() {
-  const allCards = [
-    { card_id: "#017", name: "USG-45", rarity: "Rare", filename: "017_USG45_Attack.png" },
-    { card_id: "#126", name: "Lt. Col. Emil Borén", rarity: "Legendary", filename: "126_Lt.Col.EmilBoren_Specialty.png" },
-    { card_id: "#001", name: "M4-A1", rarity: "Rare", filename: "001_M4A1_Attack.png" },
-    { card_id: "#061", name: "Binoculars", rarity: "Rare", filename: "061_Binoculars_Tactical.png" },
-    { card_id: "#042", name: "Tactical Shirt", rarity: "Common", filename: "042_TacticalShirt_Defense.png" },
-    { card_id: "#079", name: "Box of Nails", rarity: "Rare", filename: "079_BoxofNails_Loot.png" },
-    { card_id: "#077", name: "MRE", rarity: "Common", filename: "077_MRE_Loot.png" },
-    { card_id: "#023", name: "AK-74", rarity: "Rare", filename: "023_AK74_Attack.png" },
-    { card_id: "#032", name: "Ballistic Helmet", rarity: "Uncommon", filename: "032_BallisticHelmet_Defense.png" },
-    { card_id: "#033", name: "Assault Helmet", rarity: "Uncommon", filename: "033_AssaultHelmet_Defense.png" },
-    { card_id: "#114", name: "Explosive Grenade Trap", rarity: "Common", filename: "114_ExplosiveGrenadeTrap_Trap.png" },
-    { card_id: "#110", name: "Perimeter Trap", rarity: "Common", filename: "110_PerimeterTrap_Trap.png" },
-    { card_id: "#047", name: "Riders Jacket", rarity: "Rare", filename: "047_RidersJacket_Defense.png" },
-    { card_id: "#125", name: "Elena Kovak", rarity: "Legendary", filename: "125_ElenaKovak_Specialty.png" },
-    { card_id: "#058", name: "Boonie Hat", rarity: "Rare", filename: "058_BoonieHat_Defense.png" },
-    { card_id: "#096", name: "Hazmat Infected", rarity: "Uncommon", filename: "096_HazmatInfected_Infected.png" },
-    { card_id: "#098", name: "Hunter Infected", rarity: "Uncommon", filename: "098_HunterInfected_Infected.png" },
-    { card_id: "#043", name: "Paramedic Jacket", rarity: "Uncommon", filename: "043_ParamedicJacket_Defense.png" },
-    { card_id: "#045", name: "Firefighter Pants", rarity: "Common", filename: "045_FirefighterPants_Defense.png" },
-    { card_id: "#083", name: "Box of Ammo", rarity: "Common", filename: "083_BoxofAmmo_Loot.png" },
-    { card_id: "#036", name: "NBC Suit", rarity: "Common", filename: "036_NBCSuit_Defense.png" },
-    { card_id: "#108", name: "Landmine", rarity: "Rare", filename: "108_Landmine_Trap.png" },
-    { card_id: "#081", name: "Dynamite", rarity: "Rare", filename: "081_Dynamite_Loot.png" },
-    { card_id: "#019", name: "KA-74", rarity: "Uncommon", filename: "019_KA74_Attack.png" },
-    { card_id: "#049", name: "Tracksuit Jacket", rarity: "Uncommon", filename: "049_TracksuitJacket_Defense.png" },
-    { card_id: "#066", name: "Headtorch", rarity: "Rare", filename: "066_Headtorch_Tactical.png" },
-    { card_id: "#025", name: "SG5-K", rarity: "Common", filename: "025_SG5K_Attack.png" },
-    { card_id: "#089", name: "Sewing Kit", rarity: "Rare", filename: "089_SewingKit_Loot.png" },
-    { card_id: "#088", name: "Cooking Pot", rarity: "Common", filename: "088_CookingPot_Loot.png" }
-  ];
+    const allCards = [
+      { card_id: "#017", name: "USG-45", rarity: "Rare", filename: "017_USG45_Attack.png" },
+      { card_id: "#126", name: "Lt. Col. Emil Borén", rarity: "Legendary", filename: "126_Lt.Col.EmilBoren_Specialty.png" },
+      { card_id: "#001", name: "M4-A1", rarity: "Rare", filename: "001_M4A1_Attack.png" },
+      { card_id: "#061", name: "Binoculars", rarity: "Rare", filename: "061_Binoculars_Tactical.png" },
+      { card_id: "#042", name: "Tactical Shirt", rarity: "Common", filename: "042_TacticalShirt_Defense.png" },
+      { card_id: "#079", name: "Box of Nails", rarity: "Rare", filename: "079_BoxofNails_Loot.png" },
+      { card_id: "#077", name: "MRE", rarity: "Common", filename: "077_MRE_Loot.png" },
+      { card_id: "#023", name: "AK-74", rarity: "Rare", filename: "023_AK74_Attack.png" },
+      { card_id: "#032", name: "Ballistic Helmet", rarity: "Uncommon", filename: "032_BallisticHelmet_Defense.png" },
+      { card_id: "#033", name: "Assault Helmet", rarity: "Uncommon", filename: "033_AssaultHelmet_Defense.png" },
+      { card_id: "#114", name: "Explosive Grenade Trap", rarity: "Common", filename: "114_ExplosiveGrenadeTrap_Trap.png" },
+      { card_id: "#110", name: "Perimeter Trap", rarity: "Common", filename: "110_PerimeterTrap_Trap.png" },
+      { card_id: "#047", name: "Riders Jacket", rarity: "Rare", filename: "047_RidersJacket_Defense.png" },
+      { card_id: "#125", name: "Elena Kovak", rarity: "Legendary", filename: "125_ElenaKovak_Specialty.png" },
+      { card_id: "#058", name: "Boonie Hat", rarity: "Rare", filename: "058_BoonieHat_Defense.png" },
+      { card_id: "#096", name: "Hazmat Infected", rarity: "Uncommon", filename: "096_HazmatInfected_Infected.png" },
+      { card_id: "#098", name: "Hunter Infected", rarity: "Uncommon", filename: "098_HunterInfected_Infected.png" },
+      { card_id: "#043", name: "Paramedic Jacket", rarity: "Uncommon", filename: "043_ParamedicJacket_Defense.png" },
+      { card_id: "#045", name: "Firefighter Pants", rarity: "Common", filename: "045_FirefighterPants_Defense.png" },
+      { card_id: "#083", name: "Box of Ammo", rarity: "Common", filename: "083_BoxofAmmo_Loot.png" },
+      { card_id: "#036", name: "NBC Suit", rarity: "Common", filename: "036_NBCSuit_Defense.png" },
+      { card_id: "#108", name: "Landmine", rarity: "Rare", filename: "108_Landmine_Trap.png" },
+      { card_id: "#081", name: "Dynamite", rarity: "Rare", filename: "081_Dynamite_Loot.png" },
+      { card_id: "#019", name: "KA-74", rarity: "Uncommon", filename: "019_KA74_Attack.png" },
+      { card_id: "#049", name: "Tracksuit Jacket", rarity: "Uncommon", filename: "049_TracksuitJacket_Defense.png" },
+      { card_id: "#066", name: "Headtorch", rarity: "Rare", filename: "066_Headtorch_Tactical.png" },
+      { card_id: "#025", name: "SG5-K", rarity: "Common", filename: "025_SG5K_Attack.png" },
+      { card_id: "#089", name: "Sewing Kit", rarity: "Rare", filename: "089_SewingKit_Loot.png" },
+      { card_id: "#088", name: "Cooking Pot", rarity: "Common", filename: "088_CookingPot_Loot.png" }
+    ];
 
-  const rarityWeights = {
-    Common: 5,
-    Uncommon: 3,
-    Rare: 2,
-    Legendary: 1
-  };
+    const rarityWeights = {
+      Common: 5,
+      Uncommon: 3,
+      Rare: 2,
+      Legendary: 1
+    };
 
-  function weightedRandomCard() {
-    const pool = [];
-    allCards.forEach(card => {
-      const weight = rarityWeights[card.rarity] || 1;
-      for (let i = 0; i < weight; i++) {
-        pool.push(card);
-      }
-    });
+    function weightedRandomCard() {
+      const pool = [];
+      allCards.forEach(card => {
+        const weight = rarityWeights[card.rarity] || 1;
+        for (let i = 0; i < weight; i++) {
+          pool.push(card);
+        }
+      });
 
-    const chosen = structuredClone(pool[Math.floor(Math.random() * pool.length)]);
-    chosen.isNew = Math.random() < 0.5;
-    chosen.owned = 0; // << Set ownership to 0 to reflect a true unlock
-    return chosen;
+      const chosen = structuredClone(pool[Math.floor(Math.random() * pool.length)]);
+      chosen.isNew = Math.random() < 0.5;
+      chosen.owned = 1; // Show "Owned: 1" for new unlocks
+      return chosen;
+    }
+
+    return [weightedRandomCard(), weightedRandomCard(), weightedRandomCard()];
   }
-
-  return [weightedRandomCard(), weightedRandomCard(), weightedRandomCard()];
 }
+
+packReveal();
